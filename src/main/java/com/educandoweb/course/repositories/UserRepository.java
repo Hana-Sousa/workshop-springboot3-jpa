1231ad70d8d34fcf061cd5e3405ba1b7c6dc83a3
Hana Gabriela Albuquerque Sousa <hanags@JX6FKY2VNQ.tld>
2024-07-18 11:24:54 -0300

    Service Layer, Component Registration

--- src/main/java/com/educandoweb/course/repositories/UserRepository.java
+++ src/main/java/com/educandoweb/course/repositories/UserRepository.java
@@ -2,8 +2,11 @@ package com.educandoweb.course.repositories;
 
 import com.educandoweb.course.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 // There is a standard implementation because of JPA
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
